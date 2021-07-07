a2b50cc4efce23289991e6fcbd3db255791515f1
Persi87 <simon_black87@hotmail.co.uk>
2021-07-07 22:10:53 +0100

    commit tree b6e9dee3a332d4da0d093d3b0497c422c211590c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+    speakers.map do |speaker_name|
+          badge_maker(speaker_name)
+    end
+end 
+   
+# def batch_badge_creator(speakers)
+#    badge_message = []
+#     speakers.each do |speaker_name|
+#          badge_message << "Hello, my name is #{speaker_name}."
+#     end
+#     #badge_message
+# end
+
+def assign_rooms(speakers)
+   room_assignment = []
+    speakers.each_with_index do |name, room|
+    room_assignment << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+    end
+   room_assignment
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badges| 
+        puts badges
+    end
+    assign_rooms(attendees).each do |rooms| 
+        puts rooms
+    end
+end
